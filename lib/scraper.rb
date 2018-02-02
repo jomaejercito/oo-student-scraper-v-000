57aa0f5f622944a9ee60d1e3d494b5cd3f488e62
Joma Ejercito <jomaejercito@gmail.com>
2018-02-02 07:25:23 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -10,7 +10,7 @@ class Scraper
     
     doc.css("div.student-card").each do |card|
       student_hash = {
-        :name => card.css
+        :name => card.css("h4.student-name").text
         :location => 
         :profile_url => 
       }
